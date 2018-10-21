d0e354340c5282ce1d42197648c004db747f9c4f
Vincent77 <wentianqi77@outlook.com>
2018-10-21 16:19:48 +0800

    running

---
+++ src/base/index.js
@@ -0,0 +1,11 @@
+import Veact from '../lib/js/veact'
+
+const App = app => {
+  return (
+    <div className="App-root" >
+    jjjjjjj
+    </div>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-import Veact from './lib/veact'
+import Veact from './lib/js/veact'
+
+import App from './base'
+
+const model = {
+  title: 'Welcome to Veact App',
+  todos: [],
+  isPageLoading: true,
+} 
+
+Veact.createApp(document.getElementById('root'), model).mount(App)
