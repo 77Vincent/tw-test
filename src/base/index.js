66dee6aa48cf761a9dde9c1f08ccfe85385bb57b
Vincent77 <wentianqi77@outlook.com>
2018-10-21 16:42:17 +0800

    header styling in progress

--- src/base/index.js
+++ src/base/index.js
@@ -1,9 +1,12 @@
 import Veact from '../lib/js/veact'
 
+import Header from '../components/Header'
+import '../lib/css/reset.css'
+
 const App = app => {
   return (
     <div className="App-root" >
-    jjjjjjj
+      <Header />
     </div>
   )
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import Veact from '../../lib/js/veact'
+
+import './index.scss'
+import logo from '../../assets/images/logo.png'
+
+const Header = () => {
+  return (
+    <header className="App-header">
+      <img className="App-header-logo" src={logo} />
+    </header>
+  )
+}
+
+export default Header
