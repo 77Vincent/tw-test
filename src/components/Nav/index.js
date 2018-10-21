a4cc62c2b7da502ff6b36ebb9f4728bb95f0182c
Vincent77 <wentianqi77@outlook.com>
2018-10-22 02:12:38 +0800

    navigation bar done

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -12,12 +12,12 @@ const Nav = ({ app }) => {
         model.navigationItems.map(item => <div className="App-nav-item">{item}</div>) 
       }
 
-      <div className="App-nav-history">
+      <ul className="App-nav-history">
         <div className="App-nav-history-title">History</div>
         {
-          model.histories.map(item => <div>{item}</div>)
+          model.histories.map(item => <li className="App-nav-history-item">{item}</li>)
         }
-      </div>
+      </ul>
     </div>
   )
 }
