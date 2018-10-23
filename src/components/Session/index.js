b57837acfc8ad2d95c09543925ae61392bf7477a
vwen <Vincent.Wen@adp.com>
2018-10-23 12:30:51 +0800

    session bottom block in progress

--- src/components/Session/index.js
+++ src/components/Session/index.js
@@ -3,7 +3,7 @@ import Veact from '../../lib/js/veact'
 import Icon from '../Icon'
 import './index.scss'
 
-export default ({ session }) => {
+export default ({ session, app }) => {
   return (
     <div className="App-session">
       <div className="App-session-image"></div>
@@ -16,6 +16,18 @@ export default ({ session }) => {
         <div className="App-session-ip"> {session.ip} </div>
         <Icon opacity={0.5} />
         <div className="App-session-path"> {session.path} </div>
+      </div>
+
+      <div className="App-session-block-bottom">
+        <div className="App-session-add"><Icon /></div>
+        {
+          session.browsers.map(browser => (
+            <div className="App-session-browser">
+              {app.model.browserIndex[browser].name}
+              <Icon />
+            </div>
+          ))
+        }
       </div>
     </div>
   )
