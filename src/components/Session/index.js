84a8b0d69ba3626b49c90f683de805060e5f988c
vwen <Vincent.Wen@adp.com>
2018-10-23 14:32:28 +0800

    session layout done

--- src/components/Session/index.js
+++ src/components/Session/index.js
@@ -9,27 +9,33 @@ export default ({ session, app }) => {
     <div className="App-session">
       <div className="App-session-image"></div>
 
-      <div className="App-session-block-top">
-        <Icon opacity={0.5} />
-        <div className="App-session-url"> {session.url} </div>
-        <div className={`App-session-status App-session-status-${session.status}`}> {session.status} </div>
-        <Icon opacity={0.5} />
-        <div className="App-session-ip"> {session.ip} </div>
-        <Icon opacity={0.5} />
-        <div className="App-session-path"> {session.path} </div>
-      </div>
+      <div className="App-session-block-right">
+        <div className="App-session-block-top">
+          <Icon opacity={0.5} />
+          <div className="App-session-url"> {session.url} </div>
+          <div className="App-session-status-wrap">
+            <div className={`App-session-status App-session-status-${session.status}`}> {session.status} </div>
+          </div>
 
-      <div className="App-session-block-bottom">
-        <div className="App-session-add"><Icon /></div>
-        {
-          session.browsers.map(browser => (
-            <div className="App-session-browser">
-              <span>{app.model.browserIndex[browser].name}</span>
-              <Icon size="s"/>
-            </div>
-          ))
-        }
-        <Button className="App-session-deny"> Deny </Button>
+          <Icon opacity={0.5} />
+          <div className="App-session-ip"> {session.ip} </div>
+
+          <Icon opacity={0.5} />
+          <div className="App-session-path"> {session.path} </div>
+        </div>
+
+        <div className="App-session-block-bottom">
+          <div className="App-session-add"><Icon /></div>
+          {
+            session.browsers.map(browser => (
+              <div className="App-session-browser">
+                <span>{app.model.browserIndex[browser].name}</span>
+                <Icon size="s"/>
+              </div>
+            ))
+          }
+          <Button className="App-session-deny"> Deny </Button>
+        </div>
       </div>
     </div>
   )
